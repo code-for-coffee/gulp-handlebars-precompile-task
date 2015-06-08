47b778dcb149d33b3cb7c1f9a3012fe3097c15e4
James Traver <jtraverjr@gmail.com>
2015-06-07 20:06:47 -0500

    keep on movin movin movin

--- app/app.js
+++ app/app.js
@@ -1,3 +1,16 @@
 var hhspike = hhspike || {};
 hhspike.models = hhspike.models || {};
 hhspike.templates = handlebars.templates;
+
+// configure
+Handlebars.registerHelper(':', function(context, options) {
+
+  console.log(": helper called!");
+  console.log(context);
+  console.log(options);
+
+  return new Handlebars.SafeString(
+      '<span class="">'
+      + options.fn(this)
+      + '</span>');
+});
